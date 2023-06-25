79e8f0eb81be10608ac2c8afd03d0a9c2fae051c
PawelBudniak <pawel.b714@gmail.com>
2023-06-25 18:14:08 +0200

    jest bardzo ladne i dziala

--- film.js
+++ film.js
@@ -14,19 +14,23 @@ chrome.storage.sync.get('notes', function(result) {
 });
 
 
-seen = false
-// the container doesn't exist at DOM load event, so we observe mutations
-var observer = new MutationObserver(function(mutations) {
-  // Check if the "Chcę obejrzeć" button exists
-  var container = document.querySelector('.filmActionBox__card'); // Replace with an appropriate selector
-  if (container && !seen) {
-    seen = true
-    console.log("namierzony filmActionBox__card")
-    // Attach event listener to the button
-    container.appendChild(textBox)    
-    // Disconnect the MutationObserver since the button has been found
-    observer.disconnect();
-  }
-});
-observer.observe(document.body, { childList: true, subtree: true });
+// seen = false
+// // the container doesn't exist at DOM load event, so we observe mutations
+// var observer = new MutationObserver(function(mutations) {
+//   // Check if the "Chcę obejrzeć" button exists
+//   var container = document.querySelector('.filmActionBox__card'); // Replace with an appropriate selector
+//   if (container && !seen) {
+//     seen = true
+//     console.log("namierzony filmActionBox__card")
+//     // Attach event listener to the button
+//     container.appendChild(textBox)    
+//     // Disconnect the MutationObserver since the button has been found
+//     observer.disconnect();
+//   }
+// });
+// observer.observe(document.body, { childList: true, subtree: true });
 
+waitForElm('.filmActionBox__card').then((elm) => {
+  console.log("namierzony filmActionBox__card")
+  elm.appendChild(textBox)    
+});
