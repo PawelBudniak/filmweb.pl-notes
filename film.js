8b4b1874ce188a3ad66fdb0e97dee662d93f5f12
PawelBudniak <pawel.b714@gmail.com>
2023-06-25 18:18:29 +0200

    jest bardzo ladne i dziala (v2)

--- film.js
+++ film.js
@@ -6,29 +6,11 @@ var textBox = defaultNoteTextbox()
 textBox.height = '200px'
 textBox.addEventListener('input', createSaveNoteCallback(filmId))
 
-
 chrome.storage.sync.get('notes', function(result) {
   var notes = result.notes || {};
   var noteText = notes[filmId] || '';
   textBox.value = noteText;
 });
-
-
-// seen = false
-// // the container doesn't exist at DOM load event, so we observe mutations
-// var observer = new MutationObserver(function(mutations) {
-//   // Check if the "Chcę obejrzeć" button exists
-//   var container = document.querySelector('.filmActionBox__card'); // Replace with an appropriate selector
-//   if (container && !seen) {
-//     seen = true
-//     console.log("namierzony filmActionBox__card")
-//     // Attach event listener to the button
-//     container.appendChild(textBox)    
-//     // Disconnect the MutationObserver since the button has been found
-//     observer.disconnect();
-//   }
-// });
-// observer.observe(document.body, { childList: true, subtree: true });
 
 waitForElm('.filmActionBox__card').then((elm) => {
   console.log("namierzony filmActionBox__card")
